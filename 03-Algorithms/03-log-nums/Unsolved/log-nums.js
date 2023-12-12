265c2ac3423aab8a768fd5a0ea31ccfc687c1bb2
Caitlin Ash <caitlinba27@gmail.com>
2023-12-12 17:23:41 -0600

    Added folders to algorithm 22 folder

---
+++ 03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -0,0 +1,10 @@
+// Write code to print all numbers from 1 to `num`
+// Assume `num` will be a positive number
+
+var logNums = function(num) {
+    numbers = [];
+    for (var i = 1; i <= num; i++) {
+        numbers.push(i);
+        }
+    return numbers;
+};
