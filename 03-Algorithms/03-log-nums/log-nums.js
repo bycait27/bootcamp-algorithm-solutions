a0967f73ef5123a1fdc38306ea63728357f2468c
Caitlin Ash <caitlinba27@gmail.com>
2023-12-12 17:27:45 -0600

    Added algorithms and test

--- 03-Algorithms/03-log-nums/log-nums.js
+++ 03-Algorithms/03-log-nums/log-nums.js
@@ -2,9 +2,4 @@
 // Assume `num` will be a positive number
 
 var logNums = function(num) {
-    numbers = [];
-    for (var i = 1; i <= num; i++) {
-        numbers.push(i);
-        }
-    return numbers;
 };
